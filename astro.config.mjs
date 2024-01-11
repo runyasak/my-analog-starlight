699e483d2b47f2eeb651b2a9dada9bd9f7b9b7a1
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2024-01-11 21:49:04 +0700

    add analog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import angular from '@analogjs/astro-angular';
 
 // https://astro.build/config
 export default defineConfig({
@@ -23,5 +24,6 @@ export default defineConfig({
 				},
 			],
 		}),
+		angular()
 	],
 });
